65b7779b949a5d6025234a5604bf907112b0001d
Emma Meline <emma.meline@edu.ece.fr>
2022-11-29 11:46:01 +0100

    coucou

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import logo from './Group2.svg';
+import photo from './photo.svg';
+import settings from './settings.svg';
 
 function App() {
   return (
-    <div className="App">
-          A vous de jouer !
+    <div className="test">
+      <img className="col-sm-4 col-md-2" src={logo} alt="logo"/>
+      <p className="col-sm-6 col-md-8 ">Lundi 26 Novembre</p>
+      <img className="col-1" id="set" src={settings} alt="settings"/>
+      <img className="col-1" id="photo" src={photo} alt="photo"/>
     </div>
   );
 }
