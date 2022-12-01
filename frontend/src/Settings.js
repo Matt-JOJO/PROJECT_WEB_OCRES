2bf8c8303d94ca7090bff3d639a43939b9286f21
Emma Meline <emma.meline@edu.ece.fr>
2022-12-01 22:18:57 +0100

    rangement dans les components

---
+++ frontend/src/Settings.js
@@ -0,0 +1,9 @@
+
+function Settings(){
+    return(
+        <div>
+            <p>Ceci est la page de paramètres</p>
+        </div>
+    )
+}
+export default Settings;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Map from './Map';
-import Pilote from './pilote';
-import Meteo from './meteo';
-import Maree from './maree';
-import Port from './port';
-import Graph from './graph';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <App />
-      <div className="container">
-        <div className="ma"><Map /></div>
-        <div className="child">
-          <div className="pi"><Pilote /></div>
-          <div className="groupe"><div className="mm">
-            <Meteo />
-            <Maree />
-          </div>
-            <div className="po"><Port /></div>
-          </div>
-
-          <div className="gr"><Graph /></div>
-        </div>
-
-      </div>
-    </div>
+    <App/>
   </React.StrictMode>
 );
 
